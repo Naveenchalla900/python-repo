3df6b19b494db1b933e9419941f0a0b0ac02731d
Naveenchalla900 <46188646+Naveenchalla900@users.noreply.github.com>
2020-06-11 15:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return " Welcome to ShunyEka Systems Private Limited,Thank you...!!"
+        return " Hello,Welcome to ShunyEka Systems Private Limited,Thank you...!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
